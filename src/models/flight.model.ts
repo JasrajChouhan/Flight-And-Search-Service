50e4ceef7ef22ad640b60e83e13500ebbfd6d3dd
JasrajChouhan <jasrajc945@gmail.com>
2024-10-11 06:56:41 +0530

    (add): flight model schema

--- src/models/flight.model.ts
+++ src/models/flight.model.ts
@@ -1,8 +1,66 @@
-import mongoose from "mongoose";
+import mongoose, { ObjectId, Schema } from 'mongoose';
 
-const flightSchema = new mongoose.Schema({
-    
-} , {timestamps : true})
+export interface IFlight {
+  flightName: string;
+  departure: Date;
+  arival: Date;
+  departureCity: ObjectId;
+  destinationCity: ObjectId;
+  flightNumber: string;
+  airplane: ObjectId;
+  airportName: ObjectId;
+}
 
-export const Flight = mongoose.model('Flight' , flightSchema)
+const flightSchema = new mongoose.Schema<IFlight>(
+  {
+    flightName: {
+      type: String,
+      required: [true, 'Please provide flight name'],
+      index: true,
+      trim: true,
+    },
 
+    flightNumber: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+
+    departure: {
+      type: Date,
+      required: [true, 'Please provide departure date/time'],
+    },
+
+    arival: {
+      type: Date,
+      required: [true, 'Please provide arival date/time'],
+    },
+
+    departureCity: {
+      type: Schema.Types.ObjectId,
+      ref: 'City',
+      required: [true, 'Provide departure city'],
+      trim: true,
+    },
+
+    destinationCity: {
+      type: Schema.Types.ObjectId,
+      ref: 'City',
+      required: [true, 'Provide destination city'],
+      trim: true,
+    },
+
+    airplane: {
+      type: Schema.Types.ObjectId,
+      ref: 'Airplane',
+    },
+
+    airportName: {
+      type: Schema.Types.ObjectId,
+      ref: 'Airport',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Flight = mongoose.model('Flight', flightSchema);
