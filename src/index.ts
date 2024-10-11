8f7261f429d5fc453315e90a77be50089c771535
JasrajChouhan <jasrajc945@gmail.com>
2024-10-11 09:21:45 +0530

    (add): middleware in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import express, { Request, Response } from 'express';
+import type { Express } from 'express';
 
 import { getEnv } from './env';
 import dbConnection from './db/databaseConnection';
+import { errorHandler } from './middlewares/errorHandler';
+import { appMiddleware } from './middlewares/app.middleware';
 
-const app = express();
+const app: Express = express();
 dbConnection();
 const port = Number(getEnv('PORT')) || 4000;
+
+//-----------------whole app middleware layer------------------
+
+app.use(appMiddleware);
+
+//---------------------------------------------------------------
+
+//-----------------whole app route starting point----------------
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>Hello at home </h1>`);
 });
+
+//---------------------------------------------------------------
+
+app.use(errorHandler);
 
 function serverReadyOrStart() {
   app.listen(port, () => {
